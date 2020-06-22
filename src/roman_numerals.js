98d20a17f47a54775fa589bc38508297df982626
jhell85 <jhell85@gmail.com>
2020-06-22 16:18:48 -0700

    finished roman numerals up to 3999

--- src/roman_numerals.js
+++ src/roman_numerals.js
@@ -6,19 +6,21 @@ const romanNumeralConverter = (num, romanNumeral) => {
     console.log("input is greater then 3999 or input is NaN");
     return;
   } 
-  let numArry = num.toString().split("")
+  
   // Termination Case = conditional called if something goes wrong (prevents an infinate loop)
   if (num === 0) {
     return romanNumeral;
   }
+  let numArry = Array.from(String(num), Number)
 
-  if (numArray.length === 1) {
-    // Roman Numerals for numbers <=9
+  // Roman Numerals for numbers <=9
+  if (numArry.length === 1) {
+   
     if (num <= 3) {
       num -= 1;
       romanNumeral += "I";
     } 
-    else if (num === 4){
+    else if (num === 4) {
       num -= 4; 
       romanNumeral += "IV";
     }
@@ -28,13 +30,53 @@ const romanNumeralConverter = (num, romanNumeral) => {
     }  
     else if (num <= 9) {
       num -= 9;
-      romanNumeral += "IX"
+      romanNumeral += "IX";
     }
-     
-  } else if (numArray.length === 2) {
-    if (numArray[0] < 4) {
-      num -=10; 
-      romanNumeral += "X"
+    // Roman Numerals >= 10 && <= 99
+  } else if (numArry.length === 2) {
+    if (numArry[0] <= 3) {
+      console.log("numArry[0] <= 3")
+      num -= 10; 
+      romanNumeral += "X";
+    }
+    else if (numArry[0] === 4){
+      num -= 40;
+      romanNumeral += "XL";
+      // console.log(`num is ........${num}`)
+      // return;
+    }
+    else if(numArry[0] <= 8){
+      console.log("numArry[0] <= 8")
+      num -= 50;
+      romanNumeral += "L";
+    }
+    else if (numArry[0] <= 9){
+      console.log("numArry[0] <= 9")
+      num -= 90;
+      romanNumeral += "XC"
+    }
+    // Roman Numerals >=100 && <= 500
+  } else if (numArry.length === 3) {
+    if (numArry[0] <= 3) {
+      num -= 100;
+      romanNumeral += "C";
+    }
+    else if (numArry[0] === 4) {
+      num -= 400; 
+      romanNumeral += "CD";
+    }
+    else if (numArry[0] <= 8) {
+      num -= 500;
+      romanNumeral += "D";
+    }
+    else if (numArry[0] <= 9) {
+      num -= 900;
+      romanNumeral += "CM"
+    }
+  } else if (numArry.length === 4) {
+    if (numArry[0] <= 3) {
+      num -= 1000; 
+      romanNumeral += "M";
     }
   }
   console.log(numArry)
